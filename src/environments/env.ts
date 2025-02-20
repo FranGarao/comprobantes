d7529042851c24a6861148b559da9574e0e3ad94
cyb3rD0z3r <garaofran@gmail.com>
2025-02-20 13:39:03 -0300

    fix

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // API_URL:
   //   'https://invoices-backend-epbtbtgvd4hnhhh3.brazilsouth-01.azurewebsites.net/api',
-  API_URL: 'http://82.29.61.148:3333'
+  API_URL: 'http://82.29.61.148:3333/api'
 };
 
